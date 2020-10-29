7cf42a4e61c0a9efda2e5a4aadba9733cff747dc
youngerheart <admin@timehub.cc>
2020-10-29 18:02:07 +0800

    remove unused file

--- src/index.js
+++ src/index.js
@@ -49,7 +49,6 @@ export function getWindow (electron, name = 'Electron') {
           }
         };
       });
-      console.error(desktopConfig)
     } else {
       desktopConfig = {
         audio: {
